f174d24f542ef50ca6215cab04ae6b8ed64c2ad9
Jellinek <jellinekbraus@gmail.com>
2022-11-07 16:40:17 +0500

    deploy

---
+++ js/930.051187f1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmy_genshin_acc"]=self["webpackChunkmy_genshin_acc"]||[]).push([[930],{930:function(n,a,c){c.r(a),c.d(a,{default:function(){return f}});var e=c(3396);const s={class:"main"},i=(0,e._)("div",{class:"main__container"},[(0,e._)("h2",null,"Я еще не придумал как переместить сюда свою стату")],-1),r=[i];function t(n,a,c,i,t,u){return(0,e.wg)(),(0,e.iD)("main",s,r)}var u=(0,e.aZ)({}),_=c(89);const l=(0,_.Z)(u,[["render",t]]);var f=l}}]);
+//# sourceMappingURL=930.051187f1.js.map
